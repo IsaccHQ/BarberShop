5160e5ddf80d4bc6386e38e44bd1539ac47a7ef3
Isacc Hernandez <isacchernandezquiroga@gmail.com>
2022-10-20 23:24:33 -0500

    Se agregó la ruta POST para actualizar datos del objeto cliente_data.json

--- api-rest/src/index.js
+++ api-rest/src/index.js
@@ -1,25 +1,42 @@
 //Intanciar del servidor
+const { application } = require('express');
 const express = require('express');
 const app = express();
 
 //Configuración del puerto
 const puerto = 8081;
 app.set('port', process.env.PORT || puerto);
+app.set('json spaces', 2);
 
 //midleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+const cliente = require('./cliente_data.json');
 
 //API
 app.get('/', (req, res) => {
     res.send("Inicio del sitio");
 });
 
-app.use(require('./rutas/clientes'));
+app.use(require('./rutas/clientes.js'));
 app.get('/clientes', (req, res) => {
-    res.send("Listado de clientes");
+    res.json(cliente)
 });
 
+//Crear un cliente
+app.post('/cliente/post', (req, res ) => {
+    const { doc_ident, nombre, apellido, email_cliente, telefono_cliente  } = req.body;
+    if(doc_ident && nombre && apellido && email_cliente && telefono_cliente) {
+        const id = cliente.length + 1;
+        const newCliente = {id, ...req.body};
+        cliente.push(newCliente);
+        res.json(cliente);
+        console.log(newCliente);
+    } else {
+        res.json('Petición erronea')
+    }
+});
 
 //Inicialización del servidor
 app.listen(app.get('port'), () => {
